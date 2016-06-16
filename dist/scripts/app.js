a21d970ce98c6c2ea54838a1c44ff859b94035d9
Virginia <vcooper_us@yahoo.com>
2016-06-16 12:41:59 -0700

    updated files.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,8 @@
 angular.module('blocJams', []);
+
+//angular.module('app', [])
+//  .controller('MainCtrl', function($scope) {
+//    /** Body of my function goes here **/
+//}).controller("Controller2", function($scope) {
+//    
+//}).fact
